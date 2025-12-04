9f813f21f22290a09678a2863df9dfaa6bc0ce26
Naveen Velicheti <vsnaveen1@gmail.com>
2025-12-03 19:55:58 -0500

    Enable AWS X-Ray tracing for end-to-end observability
    - Enable X-Ray tracing on Lambda function
    - Enable X-Ray tracing on API Gateway
    - Instrument Lambda code with X-Ray SDK
    - Add aws-xray-sdk dependency for Lambda

--- python/apigw-http-api-lambda-dynamodb-python-cdk/lambda/apigw-handler/index.py
+++ python/apigw-http-api-lambda-dynamodb-python-cdk/lambda/apigw-handler/index.py
@@ -1,6 +1,11 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
+
+patch_all()
+
 import boto3
 import os
 import json
